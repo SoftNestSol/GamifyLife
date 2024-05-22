5fa054d52eaccc9d01359fdeeb633a6dd24d7541
Caracaș Radu Nicolae <caracasradu11@gmail.com>
2024-05-22 23:29:24 +0300

    restore

--- mobileApp/App.js
+++ mobileApp/App.js
@@ -1,8 +1,8 @@
-import { Text } from "react-native";
+import { Text, } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { AuthContextProvider } from "./src/contexts/auth.context";
-import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 
 import Navbar from "./src/components/Navbar";
 //screens
@@ -10,7 +10,8 @@ import RegisterScreen from "./src/screens/RegisterScreen";
 import HomeScreen from "./src/screens/HomeScreen";
 import QuestsScreen from "./src/screens/QuestsScreen";
 import TasksScreen from "./src/screens/TasksScreen";
-
+import SuggestionsScreen from "./src/screens/SuggestionsScreen"
+import TaskScreen from "./src/screens/TaskScreen";
 
 const stack = createNativeStackNavigator();
 export default function App() {
@@ -22,10 +23,11 @@ export default function App() {
 					{/* <stack.Screen name="Register" component={RegisterScreen} /> */}
 					<stack.Screen name="Quests" component={QuestsScreen}options={{ headerShown: false }} />
 					<stack.Screen name="OngoingTasks" component={TasksScreen}options={{ headerShown: false }} />
+					<stack.Screen name="Suggestions" component={SuggestionsScreen} options={{ headerShown: false }} />
+					<stack.Screen name="Task" component={TaskScreen} options={{headerShown: false}} />
 				</stack.Navigator>
 				<Navbar/>
 			</NavigationContainer>
-			
 		</AuthContextProvider>
 	);
 }
