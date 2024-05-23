b893cab35c28f1866d0998814dc3349ccab53fcb
Iordache Andrei Tudor <andrei.iordache2017@gmail.com>
2024-05-24 00:21:39 +0300

    Update App.js

--- mobileApp/App.js
+++ mobileApp/App.js
@@ -17,67 +17,73 @@ import NewRecurrentTask from "./src/screens/NewRecurrentTask.js";
 import NewHabitCreation from "./src/screens/NewHabitCreation.js";
 import SuggestionsScreen from "./src/screens/SuggestionsScreen";
 import TaskScreen from "./src/screens/TaskScreen.js";
+import LoginScreen from "./src/screens/LoginScreen";
 
 const stack = createNativeStackNavigator();
 export default function App() {
-  return (
-    <AuthContextProvider>
-      <NavigationContainer>
-        <stack.Navigator>
-          <stack.Screen
-            name="Home"
-            component={HomeScreen}
-            options={{ headerShown: false }}
-          />
-          {/* <stack.Screen name="Register" component={RegisterScreen} /> */}
-          <stack.Screen
-            name="Quests"
-            component={QuestsScreen}
-            options={{ headerShown: false }}
-          />
-          <stack.Screen
-            name="Journey"
-            component={JourneyScreen}
-            options={{ headerShown: false }}
-          />
-          <stack.Screen
-            name="OngoingTasks"
-            component={TasksScreen}
-            options={{ headerShown: false }}
-          />
-          <stack.Screen
-            name="TasksMenu1"
-            component={TaskCreationScreen1}
-            options={{ headerShown: false }}
-          />
-          <stack.Screen
-            name="NewTaskCreation"
-            component={NewTaskCreation}
-            options={{ headerShown: false }}
-          />
-          <stack.Screen
-            name="NewHabitCreation"
-            component={NewHabitCreation}
-            options={{ headerShown: false }}
-          />
-          <stack.Screen
-            name="NewRecurrentTask"
-            component={NewRecurrentTask}
-            options={{ headerShown: false }}
-          />
-          <stack.Screen
-            name="Suggestions"
-            component={SuggestionsScreen}
-            options={{ headerShown: false }}
-          />
-          <stack.Screen
-            name="Task"
-            component={TaskScreen}
-            options={{ headerShown: false }}
-          />
-        </stack.Navigator>
-        <Navbar />
-      </NavigationContainer>
-    </AuthContextProvider>
-  );
+	return (
+		<AuthContextProvider>
+			<NavigationContainer>
+				<stack.Navigator>
+					<stack.Screen
+						name="Login"
+						component={LoginScreen}
+						options={{ headerShown: false }}
+					/>
+					<stack.Screen
+						name="Home"
+						component={HomeScreen}
+						options={{ headerShown: false }}
+					/>
+					{/* <stack.Screen name="Register" component={RegisterScreen} /> */}
+					<stack.Screen
+						name="Quests"
+						component={QuestsScreen}
+						options={{ headerShown: false }}
+					/>
+					<stack.Screen
+						name="Journey"
+						component={JourneyScreen}
+						options={{ headerShown: false }}
+					/>
+					<stack.Screen
+						name="OngoingTasks"
+						component={TasksScreen}
+						options={{ headerShown: false }}
+					/>
+					<stack.Screen
+						name="TasksMenu1"
+						component={TaskCreationScreen1}
+						options={{ headerShown: false }}
+					/>
+					<stack.Screen
+						name="NewTaskCreation"
+						component={NewTaskCreation}
+						options={{ headerShown: false }}
+					/>
+					<stack.Screen
+						name="NewHabitCreation"
+						component={NewHabitCreation}
+						options={{ headerShown: false }}
+					/>
+					<stack.Screen
+						name="NewRecurrentTask"
+						component={NewRecurrentTask}
+						options={{ headerShown: false }}
+					/>
+					<stack.Screen
+						name="Suggestions"
+						component={SuggestionsScreen}
+						options={{ headerShown: false }}
+					/>
+					<stack.Screen
+						name="Task"
+						component={TaskScreen}
+						options={{ headerShown: false }}
+					/>
+				</stack.Navigator>
+				<Navbar />
+			</NavigationContainer>
+		</AuthContextProvider>
+	);
 }
