de6e83109977f2d95f7ae08efabd2f4cf95ff9e5
Iordache Andrei Tudor <andrei.iordache2017@gmail.com>
2024-05-24 00:16:34 +0300

    Update dbContext.js

--- server/dbContext.js
+++ server/dbContext.js
@@ -2,24 +2,22 @@ const mssql = require("mssql");
 require("dotenv").config();
 
 const DRIVER = {
-
 	user: process.env.UID,
 	password: process.env.PASSWORD,
 	server: process.env.SERVER,
 	port: parseInt(process.env.PORT),
 	database: process.env.INITIAL_CATALOG,
 	options: {
-		encrypt: process.env.ENCRYPT === "yes",
+		encrypt: 1,
 		trustServerCertificate: process.env.TRUSTSERVERCERTIFICATE === "yes",
 		connectionTimeout: parseInt(process.env.CONNECTIONTIMEOUT),
 		authentification: process.env.AUTHENTIFICATION
 	}
 };
 
-
 const connect = async () => {
 	try {
-		const CONNECTION =  mssql.connect(DRIVER);
+		const CONNECTION = mssql.connect(DRIVER);
 		console.log("Connected to the database");
 		return CONNECTION;
 	} catch (err) {
