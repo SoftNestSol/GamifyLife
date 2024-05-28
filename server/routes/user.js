eb2d5a655279e86a840461660c92ad5589dd1f49
Iordache Andrei Tudor <andrei.iordache2017@gmail.com>
2024-05-28 15:47:39 +0300

    Update user.js

--- server/routes/user.js
+++ server/routes/user.js
@@ -49,6 +49,7 @@ router.delete("/:id", async (req, res) => {
 
 router.get("/tasks/:id", async (req, res) => {
 	const id = req.params.id;
+	console.log(id);
 	const tasks = await getTodayUserTasks(id);
 	res.send(tasks);
 });
